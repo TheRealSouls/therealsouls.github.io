3af588df6f6f73553f022aaa63c1a63558c4050d
Matas Roda <matas.roda@gmail.com>
2025-07-25 08:36:50 +0100

    Added Flask as one of my skills

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ window.onload = () => {
         setTimeout(typeWriter, eraseSpeed);
       } else {
         isDeleting = false;
-        setTimeout(typesWriter, speed);
+        setTimeout(typeWriter, speed);
       }
     }
   };
@@ -71,7 +71,7 @@ const projectsDiv = document.getElementById("projects");
     {
       icon: '<i class="fa-solid fa-circle-check fa-3x"></i>',
       title: "To-Do List",
-      desc: "Uses JavaScript's localStorage API to save your tasks when you refresh the website. Functional and has dark mode. <span class=\"brackets\">(so your eyes don't burn)</span>",
+      desc: `Uses JavaScript's localStorage API to save your tasks when you refresh the website. Functional and has dark mode. <span class=\"brackets\">(so your eyes don't burn!)</span>`,
       tags: [
         '<span class="tag html">HTML</span>',
         '<span class="tag css">CSS</span>',
